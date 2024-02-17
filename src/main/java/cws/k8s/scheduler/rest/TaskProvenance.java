54023eefacdcc35fe2c2bc5151ed730be4b4ade9
Leon Schäffer <leonschaeffer@me.com>
2024-02-17 12:42:09 +0100

    cherry pick with leons cws rest client

---
+++ src/main/java/cws/k8s/scheduler/rest/TaskProvenance.java
@@ -0,0 +1,30 @@
+package cws.k8s.scheduler.rest;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class TaskProvenance {
+    private String podId;
+
+    private String processName;
+
+    private String nodeName;
+
+    private int runtime;
+
+    private long inputSize; // fs_reads_total in bytes
+
+
+    public TaskProvenance(String podId, String processName, String nodeName, int runtime) {
+        this.podId = podId;
+        this.processName = processName;
+        this.nodeName = nodeName;
+        this.runtime = runtime;
+    }
+
+    public String toString() {
+        return "{pod: " + getPodId() + ", processName: " + getProcessName() + ", nodeName: " + getNodeName() + ", runtime: " + getRuntime() + ", inputSize: " + getInputSize() + "}";
+    }
+}
